6241f6ca6327077766b11d0203e0b26cd9891ef6
Justus Garbe <55301990+Nowilltolife@users.noreply.github.com>
2023-08-10 11:35:58 +0200

    Update teset.js

--- teset.js
+++ teset.js
@@ -1,11 +1,7 @@
 
 
-        push({
-            type: "rich",
-            title: "Decrypted Message",
-            color: "0x45f5f5",
-            description: revealed,
-            footer: {
-                text: "Made with ❤️ by c0dine and Sammy!",
+        function({
+            firstObject: {
+                secondObject: "Made with ❤️ by c0dine and Sammy!",
             },
         });
